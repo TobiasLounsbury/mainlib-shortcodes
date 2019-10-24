6bbcd48bf6c7bc4d731f662e67a9f904ff028e6f
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2019-10-24 09:55:05 -0700

    Added rough and dirty main-closings code block conversion from php everywhere

--- includes/config-sample.php
+++ includes/config-sample.php
@@ -1,4 +1,5 @@
 <?php
-$MAIN_DB_HOST = "localhost";
-$MAIN_DB_USER = "mainlib";
-$MAIN_DB_PASS = "password";
+define("MAIN_DB_HOST", "localhost");
+define("MAIN_DB_USER", "mainlib");
+define("MAIN_DB_PASS", "password");
+define("MAIN_DB_NAME", "mainlib_mainclosings");
--- mainlib-shortcodes.php
+++ mainlib-shortcodes.php
@@ -25,9 +25,47 @@ require_once("includes/config.php");
 
 
 
-add_shortcode( 'mainhelloworld', 'mainlib_hello_world_shortcode' );
+add_shortcode( 'mainlib-closings', 'mainlib_closings_shortcode' );
 
 
-function mainlib_hello_world_shortcode($attr) {
-  return "Hello MAIN Library Network!";
+function mainlib_closings_shortcode($attr) {
+
+  $link = mysqli_connect(MAIN_DB_HOST, MAIN_DB_USER, MAIN_DB_PASS);
+  if(!$link) {
+    return "Error connecting to mysql server: ".mysqli_error();
+  }
+  $get_my_db=mysqli_select_db($link, MAIN_DB_NAME);
+
+  $libquery = "SELECT * FROM mainclosings ORDER BY Library ASC;";
+  $libquery = mysqli_real_escape_string($link, $libquery) or die ("Query to get data from mainclosings failed: ".mysqli_connect_error( $link ));
+  $libclosings = mysqli_query( $link, $libquery );
+  $output = "";
+  $output .= "<table>";
+  $output .= "<tr>";
+  $output .= "<th>Library</th>
+            <th>Reason</th>
+            <th>Close Date</th>
+            <th>Open Date</th>
+            <th>Open Time</th>";
+  $output .= "</tr>";
+  while ($librow=mysqli_fetch_array($libclosings)) {
+    $Library=$librow["Library"]; // <-- These vars need to match the case of the DB columns
+    $Reason=$librow["Reason"];
+    $CloseDate=$librow["CloseDate"];
+    $OpenDate= $librow["OpenDate"];
+    $OpenTime= $librow["OpenTime"];
+    $phpopendate = strtotime($OpenDate);
+    $mysqlopendate = date('M d, Y', $phpopendate);
+    $phpclosedate = strtotime($CloseDate);
+    $mysqlclosedate = date('M d, Y', $phpclosedate);
+    $output .= "<tr>";
+    $output .= "<td>$Library</td>
+              <td>$Reason</td>
+              <td>$mysqlclosedate</td>
+              <td>$mysqlopendate</td>
+              <td>$OpenTime</td>";
+    $output .= "</tr>";
+  }
+  mysqli_close($link);
+  return $output;
 }
